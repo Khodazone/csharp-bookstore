31a84d7c77cefcbfbbb5fcb4aaabe86cf36807e0
Khodazone <khodazone@gmail.com>
2023-04-12 16:59:50 +0200

    Compito principale concluso

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Esercizio: inserire in hardcoded le seguenti informazioni che troviamo su Amazon del libro Clean Code:
+ * Titolo
+ * Autore
+ * ISBN
+ * Numero pagine
+ * Peso del libro
+ * Larghezza, altezza e profondità del libro
+ * Valutazione media
+ * Numero di recensioni
+ * Se è disponibile in versione Kindle
+ * Se la copertina flessibile è disponibile
+ */
+
+// Lista dei dati del Libro
+string title = "Clean Code: A Handbook of Agile Software Craftsmanship";
+string author = "Robert C. Martin";
+long isbn = 9780132350884;
+int numberOfPages = 431;
+int weight = 800;
+float width = 17.78f;
+float height = 2.79f;
+float depth = 23.37f;
+float averageRating = 4.7f;
+int numberOfReviews = 5414;
+string kindle = "Formato Kindle disponibile";
+string flexibleCover = "Copertina flessibile disponibile";
+
+// Stampare le informazioni in console
+Console.WriteLine("-------- IL LIBRO DI OGGI: " + title + " di " + author + " ------------");
+Console.WriteLine("Informazioni generiche:");
+Console.WriteLine("ISBN: " + isbn);
+Console.WriteLine("Numero delle pagine: " + numberOfPages + " pagine");
+Console.WriteLine("Peso del libro: " + weight + " g");
+Console.WriteLine("Dimensioni del libro: " + width + " cm " + height + " cm " + depth + " cm ");
+Console.WriteLine();
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + numberOfReviews + " recensioni");
+Console.WriteLine("Valutazione media: " + averageRating + " stelline");
+Console.WriteLine("Kindle: " + kindle);
+Console.WriteLine("Copertina: " + flexibleCover);
+Console.WriteLine("---------------------------------------------------------------------------");
